7abb5400961b34bcbd7fcf5c90914e57ac37a911
Truong Van Quy <truongvanquy2k3@gmail.com>
2024-03-14 11:36:52 +0700

    add skeleton for products page

--- src/views/admin/pages/Products/Products.vue
+++ src/views/admin/pages/Products/Products.vue
@@ -1,166 +1,298 @@
 <template>
   <div class="product fluid-container mx-5" v-if="currentAccount?.permissions.includes('read_products')">
-    <div class="product-title my-4 d-flex align-items-center">
-      <h4 class="gray-text">eCommerce</h4>
-      <h5 class="mx-1">/</h5> 
-      <h4 class="primary-text">Product List</h4>
-    </div>
-
-    <div class="card mb-3">
-      <div class="card-header d-flex justify-content-between">
-        <h5 class="my-0 mt-ms-auto d-flex align-items-center">Filters</h5>
-        <div @click="handleClear" class="icon">
-          <i class="fa-solid fa-filter-circle-xmark fa-lg"></i>
-        </div>
+    <div class="is-fetching" v-if="!isFetching">
+      <div class="product-title my-4 d-flex align-items-center">
+        <h4 class="gray-text">eCommerce</h4>
+        <h5 class="mx-1">/</h5> 
+        <h4 class="primary-text">Product List</h4>
       </div>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-6">
-            <FilterStatus 
-              v-model:activedStatus="filter.status"
-            />
-          </div>
-          <div class="col-6">
-            <Search 
-              v-model:keyword="filter.keyword"
-            />
+
+      <div class="card mb-3">
+        <div class="card-header d-flex justify-content-between">
+          <h5 class="my-0 mt-ms-auto d-flex align-items-center">Filters</h5>
+          <div @click="handleClear" class="icon">
+            <i class="fa-solid fa-filter-circle-xmark fa-lg"></i>
           </div>
         </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-6">
+              <FilterStatus 
+                v-model:activedStatus="filter.status"
+              />
+            </div>
+            <div class="col-6">
+              <Search 
+                v-model:keyword="filter.keyword"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
 
-    <div class="card mb-3">
-      <div class="card-header d-flex justify-content-between">
-        <h5 class="my-0 d-flex align-items-center">Products</h5>
-        <div class="d-flex">
-          <SelectCategory
-            v-model:selectedCategory="filter.category" 
-          />
+      <div class="card mb-3">
+        <div class="card-header d-flex justify-content-between">
+          <h5 class="my-0 d-flex align-items-center">Products</h5>
+          <div class="d-flex">
+            <SelectCategory
+              v-model:selectedCategory="filter.category" 
+            />
+          </div>
         </div>
-      </div>
-      <div class="card-body">
-        <div class="product-action d-flex justify-content-between">
-          <ChangeMulti @apply="handleChangeMulti"/>
-          <router-link :to="{ name: 'CreateProduct' }">
-            <button class="btn btn-main btn-primary">+ New Product</button>
-          </router-link>
-        </div>
-        <table class="table table-sm mt-3">
-          <thead>
-            <tr>
-              <th>
-                <input
-                  class="form-check-input" 
-                  type="checkbox"
-                  name="checkall"
-                  @change="handleCheckAll"
-                  v-model="checkall"
-                >
-              </th>
-              <th>STT</th>
-              <th>Image</th>
-              <th>Title</th>
-              <th>Price</th>
-              <th>Position</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody v-if="products.length">
-            <tr v-for="product, index in products" :key="product._id">
-              <td>
-                <input 
-                  class="form-check-input"
-                  type="checkbox"
-                  name="id"
-                  :value="product._id"
-                  v-model="checkedItems[index]"
-                >
-              </td>
-              <td>{{ index + 1 }}</td>
-              <td>
-                <div class="thumbnail">
-                  <img 
-                    :src="product.thumbnail" 
-                    :alt="product.title"
+        <div class="card-body">
+          <div class="product-action d-flex justify-content-between">
+            <ChangeMulti @apply="handleChangeMulti"/>
+            <router-link :to="{ name: 'CreateProduct' }">
+              <button class="btn btn-main btn-primary">+ New Product</button>
+            </router-link>
+          </div>
+          <table class="table table-sm mt-3">
+            <thead>
+              <tr>
+                <th>
+                  <input
+                    class="form-check-input" 
+                    type="checkbox"
+                    name="checkall"
+                    @change="handleCheckAll"
+                    v-model="checkall"
                   >
-                </div>
-                
-              </td>
-              <td>{{ product.title }}</td>
-              <td>${{ product.price }}</td>
-              <td>
-                <input 
-                  type="number"
-                  name="position"
-                  v-model="product.position"
-                  :style="{ 
-                    width: '60px', 
-                    padding: '3px', 
-                  }"
-                >
-              </td>
-              <td>
-                <button
-                  v-if="product.status === 'active'"
-                  class="btn btn-sm btn-success btn-active"
-                  @click="handleChangeStatus(product)"
-                >
-                  Active
-                </button>
-                <button
-                  v-else
-                  class="btn btn-sm btn-danger btn-inactive"
-                  @click="handleChangeStatus(product)"
-                >
-                  Inactive
-                </button>
-              </td>
-              <td>
-                <div class="d-flex icon">
-                  <router-link 
-                    :to="{ name: 'ModifyProduct', params: { slug: `${product.slug}`} }"
-                    class="d-flex align-items-center"
+                </th>
+                <th>STT</th>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Position</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody v-if="products.length">
+              <tr v-for="product, index in products" :key="product._id">
+                <td>
+                  <input 
+                    class="form-check-input"
+                    type="checkbox"
+                    name="id"
+                    :value="product._id"
+                    v-model="checkedItems[index]"
                   >
-                    <i class="fa-regular fa-pen-to-square fa-lg fa-fw"></i>
-                  </router-link>
-                  <div
-                    class="ms-2"
-                    @click="handleDelete(product._id)"
-                  >
-                    <i class="fa-regular fa-trash-can fa-lg fa-fw"></i>
+                </td>
+                <td>{{ index + 1 }}</td>
+                <td>
+                  <div class="thumbnail">
+                    <img 
+                      :src="product.thumbnail" 
+                      :alt="product.title"
+                    >
                   </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-          <tbody v-else>
-            <tr>
-              <td>
-                <div class="text-center">
-                  No result.
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div class="text-center">
-          <v-container>
-            <v-row justify="center">
-              <v-col cols="8">
-                <v-container class="max-width p-0">
-                  <v-pagination
-                    v-model="page"
-                    :length="totalPages"
-                    :total-visible="6"
-                  ></v-pagination>
-                </v-container>
-              </v-col>
-            </v-row>
-          </v-container>
+                  
+                </td>
+                <td>{{ product.title }}</td>
+                <td>${{ product.price }}</td>
+                <td>
+                  <input 
+                    type="number"
+                    name="position"
+                    v-model="product.position"
+                    :style="{ 
+                      width: '60px', 
+                      padding: '3px', 
+                    }"
+                  >
+                </td>
+                <td>
+                  <button
+                    v-if="product.status === 'active'"
+                    class="btn btn-sm btn-success btn-active"
+                    @click="handleChangeStatus(product)"
+                  >
+                    Active
+                  </button>
+                  <button
+                    v-else
+                    class="btn btn-sm btn-danger btn-inactive"
+                    @click="handleChangeStatus(product)"
+                  >
+                    Inactive
+                  </button>
+                </td>
+                <td>
+                  <div class="d-flex icon">
+                    <router-link 
+                      :to="{ name: 'ModifyProduct', params: { slug: `${product.slug}`} }"
+                      class="d-flex align-items-center"
+                    >
+                      <i class="fa-regular fa-pen-to-square fa-lg fa-fw"></i>
+                    </router-link>
+                    <div
+                      class="ms-2"
+                      @click="handleDelete(product._id)"
+                    >
+                      <i class="fa-regular fa-trash-can fa-lg fa-fw"></i>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+            <tbody v-else>
+              <tr>
+                <td>
+                  <div class="text-center">
+                    No result.
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="text-center">
+            <v-container>
+              <v-row justify="center">
+                <v-col cols="8">
+                  <v-container class="max-width p-0">
+                    <v-pagination
+                      v-model="page"
+                      :length="totalPages"
+                      :total-visible="6"
+                    ></v-pagination>
+                  </v-container>
+                </v-col>
+              </v-row>
+            </v-container>
+          </div>
         </div>
       </div>
     </div>
+    <div class="is-fetching" v-else>
+      <v-row>
+        <v-col cols="6">
+          <v-skeleton-loader
+            color="grey-lighten-5"
+            class="my-2"
+            max-width="400"
+            type="heading"
+          ></v-skeleton-loader>
+        </v-col>
+      </v-row>
+
+      <div class="card mb-3">
+        <div class="card-header d-flex justify-content-between">
+          <v-row>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="my-0"
+                max-width="120"
+                type="subtitle"
+              ></v-skeleton-loader>
+            </v-col>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="ms-auto my-0"
+                max-width="120"
+                type="subtitle"
+              ></v-skeleton-loader>
+            </v-col>
+          </v-row>
+        </div>
+        <div class="card-body">
+          <v-row>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="my-0"
+                max-width="300"
+                type="heading"
+              ></v-skeleton-loader>
+            </v-col>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="ms-auto my-0"
+                max-width="400"
+                type="heading"
+              ></v-skeleton-loader>
+            </v-col>
+          </v-row>
+        </div>
+      </div>
+
+      <div class="card mb-3">
+        <div class="card-header d-flex justify-content-between">
+          <v-row>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="my-0"
+                max-width="120"
+                type="subtitle"
+              ></v-skeleton-loader>
+            </v-col>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="ms-auto my-0"
+                max-width="120"
+                type="subtitle"
+              ></v-skeleton-loader>
+            </v-col>
+          </v-row>
+        </div>
+        <div class="card-body">
+          <v-row>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="my-0"
+                max-width="300"
+                type="heading"
+              ></v-skeleton-loader>
+            </v-col>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="ms-auto my-0"
+                max-width="400"
+                type="heading"
+              ></v-skeleton-loader>
+            </v-col>
+          </v-row>
+          <table class="table table-sm mt-3">
+            <thead>
+              <tr>
+                <th colspan="12">
+                  <v-skeleton-loader
+                    class="my-0"
+                    type="table-row"
+                  ></v-skeleton-loader>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td colspan="12">
+                  <v-skeleton-loader
+                    class="my-0"
+                    type="table-row-divider@6"
+                  ></v-skeleton-loader>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="text-center">
+            <v-container>
+              <v-row justify="center">
+                <v-col cols="8" class="p-0">
+                  <v-container class="max-width p-0">
+                    <v-skeleton-loader
+                      class="mx-auto"
+                      max-width="300"
+                      type="heading"
+                    ></v-skeleton-loader>
+                  </v-container>
+                </v-col>
+              </v-row>
+            </v-container>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="product fluid-container mx-5" v-else>
+    <Unauthorized />
   </div>
 </template>
 
@@ -174,6 +306,7 @@
   import successDialogHelper from '@/helpers/admin/dialogs/success.helper.js'
   import { mapState } from 'pinia'
   import { useAuthStore } from '@/stores/admin/auth.store'
+  import Unauthorized from '@/components/admin/Unauthorized/Unauthorized.vue'
 
   export default {
     name: "Product",
@@ -182,6 +315,7 @@
       Search,
       SelectCategory,
       ChangeMulti,
+      Unauthorized,
     },
     data() {
       return {
@@ -194,7 +328,8 @@
         checkall: false,
         checkedItems: [],
         page: 1,
-        totalPages: 0
+        totalPages: 0,
+        isFetching: true
       }
     },
     methods: {
@@ -209,7 +344,8 @@
           }
         })
         this.products = data.products
-        this.totalPages = data.totalPages
+        this.totalPages = data.totalPages,
+        this.isFetching = false
       },
       handleClear() {
         this.filter = {
