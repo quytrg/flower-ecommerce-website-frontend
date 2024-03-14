32ab6132c6396322600bf3ec726dd602e61dbb49
Truong Van Quy <truongvanquy2k3@gmail.com>
2024-03-14 11:05:37 +0700

    fix skeleton and permission for accounts pages

--- src/views/admin/pages/Accounts/Accounts.vue
+++ src/views/admin/pages/Accounts/Accounts.vue
@@ -1,279 +1,281 @@
 <template>
-  <div class="accounts fluid-container mx-5" v-if="permissions?.includes('read_accounts')">
-    <div class="accounts-title my-4 d-flex align-items-center">
-      <h4 class="gray-text">Account Settings</h4>
-      <h5 class="mx-1">/</h5> 
-      <h4 class="primary-text">Account</h4>
-    </div>
-
-    <div class="card mb-3">
-      <div class="card-header d-flex justify-content-between">
-        <h5 class="my-0 mt-ms-auto d-flex align-items-center">Filters</h5>
-        <div @click="handleClear" class="icon">
-          <i class="fa-solid fa-filter-circle-xmark fa-lg"></i>
-        </div>
+  <div class="accounts fluid-container mx-5" v-if="currentAccount?.permissions.includes('read_accounts')">
+    <div class="is-fetching" v-if="!isFetching">
+      <div class="accounts-title my-4 d-flex align-items-center">
+        <h4 class="gray-text">Account Settings</h4>
+        <h5 class="mx-1">/</h5> 
+        <h4 class="primary-text">Account</h4>
       </div>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-6">
-            <FilterStatus 
-              v-model:activedStatus="filter.status"
-            />
-          </div>
-          <div class="col-6">
-            <Search 
-              v-model:keyword="filter.keyword"
-            />
+
+      <div class="card mb-3">
+        <div class="card-header d-flex justify-content-between">
+          <h5 class="my-0 mt-ms-auto d-flex align-items-center">Filters</h5>
+          <div @click="handleClear" class="icon">
+            <i class="fa-solid fa-filter-circle-xmark fa-lg"></i>
           </div>
         </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-6">
+              <FilterStatus 
+                v-model:activedStatus="filter.status"
+              />
+            </div>
+            <div class="col-6">
+              <Search 
+                v-model:keyword="filter.keyword"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
 
-    <div class="card mb-3">
-      <div class="card-header d-flex justify-content-between">
-        <h5 class="my-0 d-flex align-items-center">Accounts</h5>
-        <div class="d-flex">
-          <!-- <SelectCategory
-            v-model:selectedCategory="filter.category" 
-          /> -->
+      <div class="card mb-3">
+        <div class="card-header d-flex justify-content-between">
+          <h5 class="my-0 d-flex align-items-center">Accounts</h5>
+          <div class="d-flex">
+            <!-- <SelectCategory
+              v-model:selectedCategory="filter.category" 
+            /> -->
+          </div>
         </div>
-      </div>
-      <div class="card-body">
-        <div class="account-action d-flex justify-content-between">
-          <!-- <ChangeMulti @apply="handleChangeMulti"/> -->
-          <router-link :to="{ name: 'CreateAccount' }">
-            <button class="btn btn-main btn-primary">+ New Product</button>
-          </router-link>
-        </div>
-        <table class="table table-sm mt-3">
-          <thead>
-            <tr>
-              <th>
-                <input 
-                  class="form-check-input" 
-                  type="checkbox"
-                  name="checkall"
-                  @change="handleCheckAll"
-                  v-model="checkall"
-                >
-              </th>
-              <th>STT</th>
-              <th>Avatar</th>
-              <th>Full Name</th>
-              <th>Role</th>
-              <th>Email</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody v-if="accounts.length">
-            <tr v-for="account, index in accounts" :key="account._id">
-              <td>
-                <input 
-                  class="form-check-input" 
-                  type="checkbox"
-                  name="id"
-                  :value="account._id"
-                  v-model="checkedItems[index]"
-                >
-              </td>
-              <td>{{ index + 1 }}</td>
-              <td>
-                <div class="thumbnail">
-                  <img 
-                    :src="account.avatar" 
-                    :alt="account.fullName"
+        <div class="card-body">
+          <div class="account-action d-flex justify-content-between">
+            <!-- <ChangeMulti @apply="handleChangeMulti"/> -->
+            <router-link :to="{ name: 'CreateAccount' }">
+              <button class="btn btn-main btn-primary">+ New Product</button>
+            </router-link>
+          </div>
+          <table class="table table-sm mt-3">
+            <thead>
+              <tr>
+                <th>
+                  <input 
+                    class="form-check-input" 
+                    type="checkbox"
+                    name="checkall"
+                    @change="handleCheckAll"
+                    v-model="checkall"
                   >
-                </div>
-              </td>
-              <td>{{ account.fullName }}</td>
-              <td>{{ getRoleName(account) }}</td>
-              <td>{{ account.email }}</td>
-              <td>
-                <button
-                  v-if="account.status === 'active'"
-                  class="btn btn-sm btn-success btn-active"
-                  @click="handleChangeStatus(account)"
-                >
-                  Active
-                </button>
-                <button
-                  v-else
-                  class="btn btn-sm btn-danger btn-inactive"
-                  @click="handleChangeStatus(account)"
-                >
-                  Inactive
-                </button>
-              </td>
-              <td>
-                <div class="d-flex icon">
-                  <router-link 
-                    :to="{ name: 'ModifyAccount', params: { id: `${account._id}` } }"
+                </th>
+                <th>STT</th>
+                <th>Avatar</th>
+                <th>Full Name</th>
+                <th>Role</th>
+                <th>Email</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody v-if="accounts.length">
+              <tr v-for="account, index in accounts" :key="account._id">
+                <td>
+                  <input 
+                    class="form-check-input" 
+                    type="checkbox"
+                    name="id"
+                    :value="account._id"
+                    v-model="checkedItems[index]"
                   >
-                    <i class="fa-regular fa-pen-to-square fa-lg fa-fw"></i>
-                  </router-link>
-                  <div
-                    class="ms-2"
-                    @click="handleDelete(account._id)"
-                  >
-                    <i class="fa-regular fa-trash-can fa-lg fa-fw"></i>
+                </td>
+                <td>{{ index + 1 }}</td>
+                <td>
+                  <div class="thumbnail">
+                    <img 
+                      :src="account.avatar" 
+                      :alt="account.fullName"
+                    >
                   </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-          <tbody v-else>
-            <tr>
-              <td>
-                <div class="text-center">
-                  No result.
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div class="text-center">
-          <v-container>
-            <v-row justify="center">
-              <v-col cols="8">
-                <v-container class="max-width p-0">
-                  <v-pagination
-                    v-model="page"
-                    :length="totalPages"
-                    :total-visible="6"
-                  ></v-pagination>
-                </v-container>
-              </v-col>
-            </v-row>
-          </v-container>
+                </td>
+                <td>{{ account.fullName }}</td>
+                <td>{{ getRoleName(account) }}</td>
+                <td>{{ account.email }}</td>
+                <td>
+                  <button
+                    v-if="account.status === 'active'"
+                    class="btn btn-sm btn-success btn-active"
+                    @click="handleChangeStatus(account)"
+                  >
+                    Active
+                  </button>
+                  <button
+                    v-else
+                    class="btn btn-sm btn-danger btn-inactive"
+                    @click="handleChangeStatus(account)"
+                  >
+                    Inactive
+                  </button>
+                </td>
+                <td>
+                  <div class="d-flex icon">
+                    <router-link 
+                      :to="{ name: 'ModifyAccount', params: { id: `${account._id}` } }"
+                    >
+                      <i class="fa-regular fa-pen-to-square fa-lg fa-fw"></i>
+                    </router-link>
+                    <div
+                      class="ms-2"
+                      @click="handleDelete(account._id)"
+                    >
+                      <i class="fa-regular fa-trash-can fa-lg fa-fw"></i>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+            <tbody v-else>
+              <tr>
+                <td>
+                  <div class="text-center">
+                    No result.
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="text-center">
+            <v-container>
+              <v-row justify="center">
+                <v-col cols="8">
+                  <v-container class="max-width p-0">
+                    <v-pagination
+                      v-model="page"
+                      :length="totalPages"
+                      :total-visible="6"
+                    ></v-pagination>
+                  </v-container>
+                </v-col>
+              </v-row>
+            </v-container>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="accounts fluid-container mx-5" v-else-if="permissions === null">
-    <v-row>
-      <v-col cols="6">
-        <v-skeleton-loader
-          color="grey-lighten-5"
-          class="my-2"
-          max-width="400"
-          type="heading"
-        ></v-skeleton-loader>
-      </v-col>
-    </v-row>
+    <div class="is-fetching" v-else>
+      <v-row>
+        <v-col cols="6">
+          <v-skeleton-loader
+            color="grey-lighten-5"
+            class="my-2"
+            max-width="400"
+            type="heading"
+          ></v-skeleton-loader>
+        </v-col>
+      </v-row>
 
-    <div class="card mb-3">
-      <div class="card-header d-flex justify-content-between">
-        <v-row>
-          <v-col cols="6" class="py-1">
-            <v-skeleton-loader
-              class="my-0"
-              max-width="120"
-              type="subtitle"
-            ></v-skeleton-loader>
-          </v-col>
-          <v-col cols="6" class="py-1">
-            <v-skeleton-loader
-              class="ms-auto my-0"
-              max-width="120"
-              type="subtitle"
-            ></v-skeleton-loader>
-          </v-col>
-        </v-row>
+      <div class="card mb-3">
+        <div class="card-header d-flex justify-content-between">
+          <v-row>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="my-0"
+                max-width="120"
+                type="subtitle"
+              ></v-skeleton-loader>
+            </v-col>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="ms-auto my-0"
+                max-width="120"
+                type="subtitle"
+              ></v-skeleton-loader>
+            </v-col>
+          </v-row>
+        </div>
+        <div class="card-body">
+          <v-row>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="my-0"
+                max-width="300"
+                type="heading"
+              ></v-skeleton-loader>
+            </v-col>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="ms-auto my-0"
+                max-width="400"
+                type="heading"
+              ></v-skeleton-loader>
+            </v-col>
+          </v-row>
+        </div>
       </div>
-      <div class="card-body">
-        <v-row>
-          <v-col cols="6" class="py-1">
-            <v-skeleton-loader
-              class="my-0"
-              max-width="300"
-              type="heading"
-            ></v-skeleton-loader>
-          </v-col>
-          <v-col cols="6" class="py-1">
-            <v-skeleton-loader
-              class="ms-auto my-0"
-              max-width="400"
-              type="heading"
-            ></v-skeleton-loader>
-          </v-col>
-        </v-row>
-      </div>
-    </div>
 
-    <div class="card mb-3">
-      <div class="card-header d-flex justify-content-between">
-        <v-row>
-          <v-col cols="6" class="py-1">
-            <v-skeleton-loader
-              class="my-0"
-              max-width="120"
-              type="subtitle"
-            ></v-skeleton-loader>
-          </v-col>
-          <v-col cols="6" class="py-1">
-            <v-skeleton-loader
-              class="ms-auto my-0"
-              max-width="120"
-              type="subtitle"
-            ></v-skeleton-loader>
-          </v-col>
-        </v-row>
-      </div>
-      <div class="card-body">
-        <v-row>
-          <v-col cols="6" class="py-1">
-            <v-skeleton-loader
-              class="my-0"
-              max-width="300"
-              type="heading"
-            ></v-skeleton-loader>
-          </v-col>
-          <v-col cols="6" class="py-1">
-            <v-skeleton-loader
-              class="ms-auto my-0"
-              max-width="400"
-              type="heading"
-            ></v-skeleton-loader>
-          </v-col>
-        </v-row>
-        <table class="table table-sm mt-3">
-          <thead>
-            <tr>
-              <th colspan="12">
-                <v-skeleton-loader
-                  class="my-0"
-                  type="table-row"
-                ></v-skeleton-loader>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td colspan="12">
-                <v-skeleton-loader
-                  class="my-0"
-                  type="table-row-divider@6"
-                ></v-skeleton-loader>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div class="text-center">
-          <v-container>
-            <v-row justify="center">
-              <v-col cols="8" class="p-0">
-                <v-container class="max-width p-0">
+      <div class="card mb-3">
+        <div class="card-header d-flex justify-content-between">
+          <v-row>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="my-0"
+                max-width="120"
+                type="subtitle"
+              ></v-skeleton-loader>
+            </v-col>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="ms-auto my-0"
+                max-width="120"
+                type="subtitle"
+              ></v-skeleton-loader>
+            </v-col>
+          </v-row>
+        </div>
+        <div class="card-body">
+          <v-row>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="my-0"
+                max-width="300"
+                type="heading"
+              ></v-skeleton-loader>
+            </v-col>
+            <v-col cols="6" class="py-1">
+              <v-skeleton-loader
+                class="ms-auto my-0"
+                max-width="400"
+                type="heading"
+              ></v-skeleton-loader>
+            </v-col>
+          </v-row>
+          <table class="table table-sm mt-3">
+            <thead>
+              <tr>
+                <th colspan="12">
                   <v-skeleton-loader
-                    class="mx-auto"
-                    max-width="300"
-                    type="heading"
+                    class="my-0"
+                    type="table-row"
                   ></v-skeleton-loader>
-                </v-container>
-              </v-col>
-            </v-row>
-          </v-container>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td colspan="12">
+                  <v-skeleton-loader
+                    class="my-0"
+                    type="table-row-divider@6"
+                  ></v-skeleton-loader>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="text-center">
+            <v-container>
+              <v-row justify="center">
+                <v-col cols="8" class="p-0">
+                  <v-container class="max-width p-0">
+                    <v-skeleton-loader
+                      class="mx-auto"
+                      max-width="300"
+                      type="heading"
+                    ></v-skeleton-loader>
+                  </v-container>
+                </v-col>
+              </v-row>
+            </v-container>
+          </div>
         </div>
       </div>
     </div>
@@ -293,6 +295,8 @@
   import RoleService from '@/services/admin/role.service'
   import confirmDialogHelper from '@/helpers/admin/dialogs/confirm.helper.js'
   import successDialogHelper from '@/helpers/admin/dialogs/success.helper.js'
+  import { mapState } from 'pinia'
+  import { useAuthStore } from '@/stores/admin/auth.store'
 
   export default {
     name: "Accounts",
@@ -315,7 +319,7 @@
         checkedItems: [],
         page: 1,
         totalPages: 0,
-        permissions: null
+        isFetching: true
       }
     },
     methods: {
@@ -324,15 +328,15 @@
           const filter = Object.fromEntries(
             Object.entries(this.filter).filter(([key, value]) => value !== '')
           );
-          const { info, permissions } = await AccountService.get({
+          const data = await AccountService.get({
             params: {
               ...filter,
               page: this.page
             }
           })
-          this.accounts = info.accounts
-          this.totalPages = info.totalPages
-          this.permissions = permissions
+          this.accounts = data.accounts
+          this.totalPages = data.totalPages
+          this.isFetching = false
         }
         catch (err) {
           console.log(err)
@@ -379,7 +383,7 @@
         catch (err) {
           console.log(err)
         }
-      }
+      },
     },
     created() {
       this.getAccounts(),
@@ -397,6 +401,9 @@
         this.getAccounts()
       }
     },
+    computed: {
+      ...mapState(useAuthStore, ['currentAccount'])
+    }
   }
 </script>
 
