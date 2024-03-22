9489e24ec37ff2d567285ae8699577adafff1d50
Truong Van Quy <truongvanquy2k3@gmail.com>
2024-03-22 18:59:18 +0700

    fix buldFormData func to be able to build an array of File

--- src/helpers/admin/convert/objectToFormData.helper.js
+++ src/helpers/admin/convert/objectToFormData.helper.js
@@ -1,8 +1,15 @@
 const buildFormData = (formData, data, parentKey) => {
   if (data && typeof data === 'object' && !(data instanceof Date) && !(data instanceof File) && !(data instanceof Blob)) {
-    Object.keys(data).forEach(key => {
-      buildFormData(formData, data[key], parentKey ? `${parentKey}[${key}]` : key);
-    });
+    if (Array.isArray(data) && data.every(item => item instanceof File)) {
+      data.forEach((file) => {
+        formData.append(`${parentKey}`, file);
+      });
+    }
+    else {
+      Object.keys(data).forEach(key => {
+        buildFormData(formData, data[key], parentKey ? `${parentKey}[${key}]` : key);
+      });
+    }
   } else {
     const value = data == null ? '' : data;
 
