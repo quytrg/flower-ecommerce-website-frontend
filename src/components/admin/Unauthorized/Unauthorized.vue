dd078c634c46c8e513af4fc79ca7caec11a0e761
Truong Van Quy <truongvanquy2k3@gmail.com>
2024-03-24 23:28:54 +0700

    fix ui of authorized component (2)

--- src/components/admin/Unauthorized/Unauthorized.vue
+++ src/components/admin/Unauthorized/Unauthorized.vue
@@ -2,8 +2,8 @@
   <div class="unauthorized d-flex justify-content-center align-items-center flex-column">
     <h1 class="mb-1">403</h1>
     <h3 class="mb-2">Forbidden</h3>
-    <h4>The page you are trying to access has restricted access</h4>
-    <h5>Contact your administator if you need help</h5>
+    <h4 class="text-center">The page you are trying to access has restricted access</h4>
+    <h5 class="text-center">Contact your administator if you need help</h5>
   </div>
 </template>
 
